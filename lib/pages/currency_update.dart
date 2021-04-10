89e5288497de8b05f7b882c4ed530434364e8a65
kubi08 <kubilay_besli@hotmail.com>
2021-04-10 23:33:38 +0300

    Date

--- lib/pages/currency_update.dart
+++ lib/pages/currency_update.dart
@@ -14,176 +14,201 @@ class CurrencyUpdatePage extends StatefulWidget {
 }
 
 class _CurrencyUpdatePageState extends State<CurrencyUpdatePage> {
-  DateTime tarih;
-  String startDate = "Başlangıç Tarihi";
+  String startDate="10/04/2020";
+  String endDate="10/02/2021";
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       resizeToAvoidBottomInset: false,
-      body: SliderMenuContainer(
-        appBarColor: Colors.white,
-        drawerIconSize: 25,
-        isTitleCenter: false,
-        appBarPadding: const EdgeInsets.only(top: 25),
-        appBarHeight: 81,
-        drawerIconColor: Colors.grey.shade700,
-        title: Text(
-          '  Para Tanımlama',
-          textAlign: TextAlign.left,
-          style: TextStyle(
-              fontSize: 18,
-              color: Colors.red,
-              fontWeight: FontWeight.w500,
-              fontFamily: 'primaryFont'),
-        ),
-        sliderMenu: SlideDrawerMenuWidget(),
-        sliderMain: SingleChildScrollView(
-          child: Container(
+      body: SingleChildScrollView(
+        child: Container(
+          margin: EdgeInsets.only(left: 15, right: 15,top: 55),
+          child: FDottedLine(
+            corner: FDottedLineCorner.all(15),
+            child: Column(
+              children: <Widget>[
+                SizedBox(height: 15),
+                Text("Kur Bilgilerini Güncelle",style: TextStyle(fontSize: 25),),
+                SizedBox(height: 20),
+                FittedBox(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      Container(
+                        height: 50,
+                        width: 320,
+                        child: FlatButton.icon(
+                          icon: Icon(Icons.monetization_on_outlined,color: secondaryColor),
+                          shape: RoundedRectangleBorder(
+                              side: new BorderSide(
+                                  color: Colors.grey.shade500, width: 1),
+                              borderRadius: BorderRadius.circular(4.0)),
 
-            margin: EdgeInsets.only(left: 15, right: 15,top: 10),
-            child: FDottedLine(
-              corner: FDottedLineCorner.all(10),
-              child: Column(
-                children: <Widget>[
-                  SizedBox(height: 10),
-                  FittedBox(
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.center,
-                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                      children: [
-                        Container(
-                          margin: EdgeInsets.only(left: 10,right: 10),
-                          height: 50,
-                          width: 320,
-                          child: FlatButton.icon(
-                            icon: Icon(Icons.monetization_on_outlined,color: secondaryColor),
-                            shape: RoundedRectangleBorder(
-                                side: new BorderSide(
-                                    color: Colors.grey.shade500, width: 1),
-                                borderRadius: BorderRadius.circular(4.0)),
-
-                            label: Padding(
-                                padding: EdgeInsets.only(left: 10),
-                                child: Row(
-                                  children: <Widget>[
-                                    DropdownMenuWidget(),
-                                  ],
-                                )),
-                          ),
+                          label: Padding(
+                              padding: EdgeInsets.only(left: 10),
+                              child: Row(
+                                children: <Widget>[
+                                  DropdownMenuWidget(),
+                                ],
+                              )),
                         ),
-                        SizedBox(height: 10),
-                        Container(
-                          margin: EdgeInsets.only(left: 10,right: 10),
-                          height: 50,
-                          width: 320,
-                          child: FlatButton.icon(
-                            icon: Icon(Icons.account_balance_outlined,color: secondaryColor,),
-                            shape: RoundedRectangleBorder(
-                                side: new BorderSide(
-                                    color: Colors.grey.shade500, width: 1),
-                                borderRadius: BorderRadius.circular(4.0)),
+                      ),
+                      SizedBox(height: 15),
+                      Container(
+                        height: 50,
+                        width: 320,
+                        child: FlatButton.icon(
+                          icon: Icon(Icons.account_balance_outlined,color: secondaryColor,),
+                          shape: RoundedRectangleBorder(
+                              side: new BorderSide(
+                                  color: Colors.grey.shade500, width: 1),
+                              borderRadius: BorderRadius.circular(4.0)),
 
-                            label: Padding(
-                                padding: EdgeInsets.only(left: 10),
-                                child: Row(
-                                  children: <Widget>[
-                                    CountryDropdownMenuWidget(),
-                                  ],
-                                )),
-                          ),
+                          label: Padding(
+                              padding: EdgeInsets.only(left: 10),
+                              child: Row(
+                                children: <Widget>[
+                                  CountryDropdownMenuWidget(),
+                                ],
+                              )),
                         ),
-                      ],
-                    ),
+                      ),
+                    ],
                   ),
-                  SizedBox(height: 10),
-                  FittedBox(
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      crossAxisAlignment: CrossAxisAlignment.center,
-                      children: [
-                        Container(
-                          margin: EdgeInsets.only(left: 10),
-                          height: 50,
-                          width: 160,
-                          child: FlatButton.icon(
-                            shape: RoundedRectangleBorder(
-                                side: BorderSide(color: Colors.grey.shade500),
-                                borderRadius:
-                                BorderRadius.all(Radius.circular(4.0))),
-                            icon: Icon(Icons.date_range_outlined),
-                            label: Text(
-                              "Başla Tarihi",
-                              style: TextStyle(color: secondaryColor, fontSize: 14),
+                ),
+                SizedBox(height: 15),
+                FittedBox(
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      Container(
+                        height: 50,
+                        width: 160,
+                        child: FlatButton.icon(
+                          shape: RoundedRectangleBorder(
+                              side: BorderSide(color: Colors.grey.shade500),
+                              borderRadius:
+                              BorderRadius.all(Radius.circular(4.0))),
+                          icon: Icon(Icons.date_range_outlined),
+                          label: Text(
+                            "Başla Tarihi",
+                            style: TextStyle(color: secondaryColor, fontSize: 14),
+                          ),
+                          color: whiteColor,
+                          onPressed: () {
+                            DatePicker.showDatePicker(context,
+                                showTitleActions: true,
+                                minTime: DateTime(2008, 1, 1),
+                                maxTime: DateTime(2020, 31, 12),
+                                onChanged: (date) {
+                                  setState(() {
+                                    startDate = date.toString().substring(0,11);
+                                  });
+
+                                  print('change $date');
+                                }, onConfirm: (date) {
+                                  print('confirm $date');
+                                },
+                                currentTime: DateTime.now(),
+                                locale: LocaleType.tr);
+                          },
+                        ),
+                      ),
+                      Container(
+                        height: 50,
+                        width: 155,
+                        child: FlatButton.icon(
+                          shape: RoundedRectangleBorder(
+                              side: BorderSide(color: Colors.grey.shade500),
+                              borderRadius:
+                              BorderRadius.all(Radius.circular(4.0))),
+                          icon: Icon(Icons.date_range_outlined),
+                          label: Text(
+                            "Bitiş Tarihi",
+                            style: TextStyle(color: secondaryColor, fontSize: 14),
+                          ),
+                          color: whiteColor,
+                          onPressed: () {
+                            DatePicker.showDatePicker(context,
+                                showTitleActions: true,
+                                minTime: DateTime(2008, 1, 1),
+                                maxTime: DateTime(2020, 31, 12),
+                                onChanged: (date) {
+                                  setState(() {
+                                    endDate = date.toString().substring(0,11);
+                                  });
+
+                                  print('change $date');
+                                }, onConfirm: (date) {
+                                  print('confirm $date');
+                                },
+                                currentTime: DateTime.now(),
+                                locale: LocaleType.tr);
+                          },
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                SizedBox(height: 5),
+                Card(
+                  elevation: 2,
+                  child: Row(
+                    children: [
+                      Container(
+                        width: 155,
+                        height: 60,
+                        child: Column(
+                          children: [
+                            Container(
+                              margin: EdgeInsets.only(top: 5),
+                              child: Text("Başlama Tarihi:",style: TextStyle(color: secondaryColor,
+                                  fontSize: 14),textAlign: TextAlign.left,),
                             ),
-                            color: whiteColor,
-                            onPressed: () {
-                              DatePicker.showDatePicker(context,
-                                  showTitleActions: true,
-                                  minTime: DateTime(2008, 1, 1),
-                                  maxTime: DateTime(2020, 31, 12),
-                                  onChanged: (date) {
-                                    setState(() {
-                                      tarih = date;
-                                    });
-
-                                    print('change $date');
-                                  }, onConfirm: (date) {
-                                    print('confirm $date');
-                                  },
-                                  currentTime: DateTime.now(),
-                                  locale: LocaleType.tr);
-                            },
-                          ),
-                        ),
-                        Container(
-                          margin: EdgeInsets.only(right: 10),
-                          height: 50,
-                          width: 155,
-                          child: FlatButton.icon(
-                            shape: RoundedRectangleBorder(
-                                side: BorderSide(color: Colors.grey.shade500),
-                                borderRadius:
-                                BorderRadius.all(Radius.circular(4.0))),
-                            icon: Icon(Icons.date_range_outlined),
-                            label: Text(
-                              "Bitiş Tarihi",
-                              style: TextStyle(color: secondaryColor, fontSize: 14),
+                            Container(
+                              margin: EdgeInsets.only(top: 5),
+                              child: Text("$startDate",style: TextStyle(color:primaryColor,
+                                  fontSize: 13),textAlign: TextAlign.left,),
                             ),
-                            color: whiteColor,
-                            onPressed: () {
-                              DatePicker.showDatePicker(context,
-                                  showTitleActions: true,
-                                  minTime: DateTime(2008, 1, 1),
-                                  maxTime: DateTime(2020, 31, 12),
-                                  onChanged: (date) {
-                                    setState(() {
-                                      tarih = date;
-                                    });
 
-                                    print('change $date');
-                                  }, onConfirm: (date) {
-                                    print('confirm $date');
-                                  },
-                                  currentTime: DateTime.now(),
-                                  locale: LocaleType.tr);
-                            },
-                          ),
+                          ],
                         ),
-                      ],
-                    ),
+                      ),
+                      Container(
+                        width: 155,
+                        height: 60,
+                        child: Column(
+                          children: [
+                            Container(
+                              margin: EdgeInsets.only(top: 5),
+                              child: Text("Bitiş Tarihi:",style: TextStyle(color: secondaryColor,
+                                  fontSize: 14),textAlign: TextAlign.left,),
+                            ),
+                            Container(
+                              margin: EdgeInsets.only(top: 5),
+                              child: Text("$endDate",style: TextStyle(color:primaryColor,
+                                  fontSize: 13),textAlign: TextAlign.left,),
+                            ),
+
+                          ],
+                        ),
+                      ),
+                    ],
                   ),
-                  //Text(tarih.toString()),
-                  SizedBox(height: 15),
-                  CustomButtonWidget(
-                    title: "Para Birimi Ekle",
-                    textColor: whiteColor,
-                    buttonBackgroundColor: primaryColor,
-                    buttonIcon: Icons.add_circle_outline_outlined,
-                  ),
-                  SizedBox(height: 20),
-                ],
-              ),
+                ),
+                //Text(tarih.toString()),
+                SizedBox(height: 15),
+                CustomButtonWidget(
+                  title: "Para Birimi Ekle",
+                  textColor: whiteColor,
+                  buttonBackgroundColor: primaryColor,
+                  buttonIcon: Icons.add_circle_outline_outlined,
+                ),
+                SizedBox(height: 20),
+              ],
             ),
           ),
         ),
